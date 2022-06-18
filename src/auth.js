bad5cab2ad954181be6e28fd5196dc1166efea16
Surya Jasper <suryajasper2004@gmail.com>
2022-06-18 23:17:16 +0000

    Modified Authentication System

--- src/auth.js
+++ src/auth.js
@@ -1,11 +1,21 @@
 import Cookies from "./cookies";
 
+function randomStr(length) {
+  var randomChars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  var result = '';
+  for ( var i = 0; i < length; i++ ) {
+      result += randomChars.charAt(Math.floor(Math.random() * randomChars.length));
+  }
+  return result;
+}
+
 async function getUid() {
 
   if (Cookies.get('uid')) return Cookies.get('uid');
   
   const { ip_address } = await fetch('https://ipgeolocation.abstractapi.com/v1/?api_key=fbd6af3c2e144315889e022edff639a1');
-  const uid = btoa(ip_address);
+  //console.log('ip', ip_address);
+  const uid = btoa(ip_address) + randomStr(7);
   
   Cookies.set('uid', uid);
   return uid;
